37d08537868959ca74ee7e58aaf6b8d76bf048d1
Vladimir Marsov <marsov012@gmail.com>
2024-01-22 20:22:40 +0200

    nginx added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    // output: 'export',
-    server: {
-        port: process.env.PORT || 3000,
-    },
 }
 
-const withNextIntl = require('next-intl/plugin')();
-//module.exports = withNextIntl(nextConfig);
 module.exports = nextConfig;
