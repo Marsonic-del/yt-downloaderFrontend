01ed866a43323bbd21234787e49fdc15ca53f172
Vladimir Marsov <marsov012@gmail.com>
2024-02-14 22:44:13 +0200

    sitemap updated

--- app/[lang]/sitemap.ts
+++ app/[lang]/sitemap.ts
@@ -3,7 +3,7 @@ import { BASE_URL } from '@/app/lib/constants'
 
 let locales = ['en', 'ru', 'hu', 'pl', 'cz', 'de', 'fr', 'it', 'es', 'pt', 'dk', 'no', 'fi', 'se', 'nl', 'lv', 'lt', 'ee', 'in', 'jp', 'en-US', 'en-GB', 'ar', 'bg', 'el', 'he', 'id', 'ko', 'ro', 'sk', 'sl', 'th', 'tr', 'uk', 'vi', 'zh', 'zh', 'zh-SG', 'zh-TW', 'zh-HK']
 
-const routes =[ '', '/mp3-converter', '/mp4-converter', '/privacy-policy', '/terms-of-use', '/download-youtube-shorts', '/contact-us']
+const routes =[ '', '/mp3-converter', '/mp4-converter', '/privacy-policy', '/terms-of-use', '/download-youtube-shorts', '/contact-us', '/formats/mp3', '/formats/mp4', '/formats/mp3', '/formats/webm', '/formats/3gp', '/formats/mp4a', '/formats/hd']
  
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrls =  locales.map((locale) => {
@@ -13,6 +13,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
    return baseUrls.flatMap(url => routes.map(route => ({
     url: `${url}${route}`,
     changeFrequency: 'weekly',
-    lastModified: new Date('February 8, 2024 15:15:30'),
+    lastModified: new Date('February 14, 2024 15:15:30'),
    })));
   }
