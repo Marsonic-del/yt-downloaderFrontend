bcf82b37b238132f45ad9ec14bed7c78c4a40b19
Vladimir Marsov <marsov012@gmail.com>
2024-02-15 00:36:40 +0200

    bug fixing in renderToString

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import ReactDOMServer from 'next/dist/compiled/react-dom/cjs/react-dom-server-legacy.browser.development';
+import ReactDOMServer from 'next/dist/compiled/react-dom/cjs/react-dom-server-legacy.browser.production';
+// dev 'next/dist/compiled/react-dom/cjs/react-dom-server-legacy.browser.development'
 // prod mode `next/dist/build/noop-react-dom-server-legacy`
 
 export const formats = ['MP3', 'MP4', 'MP4A', 'Webm', '3GP', 'HD'];
